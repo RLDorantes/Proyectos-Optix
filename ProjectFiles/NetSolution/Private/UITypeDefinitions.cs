116ec0c3e7c8f3a5ff72351b025c1012dbc7b1b4
Rafael Dorantes <115169044+RLDorantes@users.noreply.github.com>
2024-06-10 22:57:41 -0400

    Add files via upload

---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,101 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "632709bc0ca708c922da2f68cb20eb09")]
+public class Diagnostics : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "4ac0e3cd4de14c3030a2b4e11526681b")]
+public class Elementos_Graficos : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "41382c57dc616862c1af276ff6942abc")]
+public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
+public class NetworkSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
+public class LAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "9cf154c923572624320f16c16fa657a3")]
+public class Configuration : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "cca9f8f75e244ad8da47837e3655e8bc")]
+public class Panel2 : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
+public class RequestAssistancePanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "f0afd51b3f49febd40b32153974e81b8")]
+public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "OptixPanel_01", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
+{
+}
